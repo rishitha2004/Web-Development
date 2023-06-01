d76bf3e0bce2b8f39b56865af839742e0067040b
rishitha2004 <rishithachowdarykonanki@gmail.com>
2023-06-01 12:25:16 +0530

    NPM

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+//Silly Name
+
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log('My name is '+sillyName);
+
+//Super Hero name
+
+// const superheroes = require('superheroes');
+ import superheroes from "superheroes";
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+console.log("I am "+superheroes.random());
